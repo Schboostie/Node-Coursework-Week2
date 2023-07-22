7246df0c8d602c3ff44ba70c8961f803d67c200c
Shahid Amin <schboostie@gmail.com>
2023-07-22 09:09:59 +0100

    -first commit server.js changes

--- server.js
+++ server.js
@@ -1,23 +1,60 @@
 const express = require("express");
-const cors = require("cors");
-
 const app = express();
+const port = 3000; // You can change this port to any value you prefer
 
-app.use(cors());
+// Middleware to parse JSON data in request body
+app.use(express.json());
 
-const welcomeMessage = {
-  id: 0,
-  from: "Bart",
-  text: "Welcome to CYF chat system!",
-};
+// Data store for chat messages
+let messages = [
+  {
+    id: 1,
+    from: "Neill",
+    text: "hi CYF! Go ahead! If you think you know how to do that, go ahead!",
+  },
+];
 
-//This array is our "data store".
-//We will start with one message in the array.
-//Note: messages will be lost when Glitch restarts our server.
-const messages = [welcomeMessage];
+// Route to create a new message
+app.post("/messages", (req, res) => {
+  const { id, from, text } = req.body;
+  if (!id || !from || !text) {
+    return res
+      .status(400)
+      .json({ error: "Please provide id, from, and text in JSON format." });
+  }
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + "/index.html");
+  const newMessage = { id, from, text };
+  messages.push(newMessage);
+  res.status(201).json(newMessage);
 });
 
-app.listen(process.env.PORT);
+// Route to read all messages
+app.get("/messages", (req, res) => {
+  res.json(messages);
+});
+
+// Route to read one message specified by an ID
+app.get("/messages/:id", (req, res) => {
+  const messageId = parseInt(req.params.id);
+  const message = messages.find((msg) => msg.id === messageId);
+  if (!message) {
+    return res.status(404).json({ error: "Message not found." });
+  }
+  res.json(message);
+});
+
+// Route to delete a message by ID
+app.delete("/messages/:id", (req, res) => {
+  const messageId = parseInt(req.params.id);
+  const index = messages.findIndex((msg) => msg.id === messageId);
+  if (index === -1) {
+    return res.status(404).json({ error: "Message not found." });
+  }
+  messages.splice(index, 1);
+  res.json({ message: "Message deleted successfully." });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Chat server listening at http://localhost:${port}`);
+});
